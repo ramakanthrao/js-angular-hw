b02f3dbf7fb2db16578b9ff9f73b40e2a39d2185
Ramakanth Kulkarni <Ramakanth_Kulkarni@epam.com>
2020-02-04 23:49:28 +0530

    testing integrety

--- bin/users.js
+++ bin/users.js
@@ -1,5 +1,5 @@
 'use strict';
-export users = [  "Venkatarao_Gude",
+const users = [  "Venkatarao_Gude",
                   "Akbar_Mohmad",
                   "Vedadri_Kakarla",
                   "Nishant_Ranjan",
@@ -14,3 +14,4 @@ export users = [  "Venkatarao_Gude",
                   "Anjaneyelu_Neerati",
                   "Rakesh_Budugu"
                 ];
+module.exports = users;              
