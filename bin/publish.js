2ec02bb7263d4f66c8fa5e3ca4d5d6c1cb0cd5b5
Ramakanth Kulkarni <Ramakanth_Kulkarni@epam.com>
2020-02-04 23:53:47 +0530

    testing integrety

--- bin/publish.js
+++ bin/publish.js
@@ -3,14 +3,15 @@ const request = require('request');
 const users = require('./users');
 const fs = require('fs');
 const myArgs = process.argv.slice(2);
+console.log('myArgs[0]:',myArgs[0]);
 let rawdata = fs.readFileSync('jasmine-test-results.json');
 let report = JSON.parse(rawdata);
 let payLoad = { user: myArgs[0], report: report };
-//let url = 'http://10.245.128.153:8080/raw';
-let url = 'http://localhost:8080/raw';
+let url = 'http://10.245.128.153:8080/raw';
+//let url = 'http://localhost:8080/raw';
 if(myArgs[0] == 'master'){
 	payLoad.userList = users;
-	url = 'http://localhost:8080/masterdata';
+	url = 'http://10.245.128.153:8080/masterdata';
 }
 console.log('sending report: ');
 request({
@@ -19,6 +20,8 @@ request({
     json: true,
     body: payLoad
 }, function (error, response, body){
-    console.log(response);
+    console.log('response:',response);
+		console.log('error:',error);
+		console.log('body:',body);
 		console.log('report sent: ');
 });
