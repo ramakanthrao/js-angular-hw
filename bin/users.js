b082bd66bcffd4f46cc6e6cc44024dad4f2c1997
Ramakanth Kulkarni <Ramakanth_Kulkarni@epam.com>
2020-02-04 19:10:27 +0530

    updated the user list

--- bin/users.js
+++ bin/users.js
@@ -11,5 +11,6 @@ export users = [  "Venkatarao_Gude",
                   "Devendar_Reddy_Gadiam",
                   "Naveen_Baddhipadige",
                   "Sridhar_Yamsani",
-                  "Anjaneyelu_Neerati"
+                  "Anjaneyelu_Neerati",
+                  "Rakesh_Budugu"
                 ];
