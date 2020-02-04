c2caa54593f8ce350880ea89e1b4f9ebfd0e9273
Ramakanth Kulkarni <Ramakanth_Kulkarni@epam.com>
2020-02-04 12:13:35 +0530

    updated userlist

--- bin/users.js
+++ bin/users.js
@@ -1,2 +1,15 @@
 'use strict';
-export users = ["Ramakanth_Kulkarni"];
+export users = [  "Venkatarao_Gude",
+                  "Akbar_Mohmad",
+                  "Vedadri_Kakarla",
+                  "Nishant_Ranjan",
+                  "Jitendra_Pandey",
+                  "Ismail_Shaik",
+                  "Karnakar_Gadegoni",
+                  "Suneetha_Pusuluri",
+                  "Sarath_Vankadaru",
+                  "Devendar_Reddy_Gadiam",
+                  "Naveen_Baddhipadige",
+                  "Sridhar_Yamsani",
+                  "Anjaneyelu_Neerati"
+                ];
