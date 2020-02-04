02c00e02c5b005ff49b194a8d9616a8f1dfae700
Ramakanth Kulkarni <ramakanth_kulkarni@epam.com>
2020-02-04 18:21:34 +0300

    Update users.js

--- bin/users.js
+++ bin/users.js
@@ -1,4 +1,16 @@
 'use strict';
 export users = [  "Venkatarao_Gude",
-                  "Akbar_Mohmad"
+                  "Akbar_Mohmad",
+                    "Vedadri_Kakarla",
+                  "Nishant_Ranjan",
+                  "Jitendra_Pandey",
+                  "Ismail_Shaik",
+                  "Karnakar_Gadegoni",
+                  "Suneetha_Pusuluri",
+                  "Sarath_Vankadaru",
+                  "Devendar_Reddy_Gadiam",
+                  "Naveen_Baddhipadige",
+                  "Sridhar_Yamsani",
+                  "Anjaneyelu_Neerati",
+                  "Rakesh_Budugu"
                 ];
