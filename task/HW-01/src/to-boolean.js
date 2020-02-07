814c96b72533f92e53d06098dd565737d71d2750
Sridhar Yamsani <Sridhar_Yamsani@epam.com>
2020-02-07 16:53:36 +0530

    task arrays

--- task/HW-01/src/to-boolean.js
+++ task/HW-01/src/to-boolean.js
@@ -13,13 +13,8 @@
  *   toBoolean(new Number(0)) => false
  */
 function toBoolean(value) {
-    if(value) {
-        return true;
-    }else{
-        return false;
-    }
-  
-}
+    return Boolean(value);
+  }
 
 module.exports = {
     toBoolean: toBoolean
