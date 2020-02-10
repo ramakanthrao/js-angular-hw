09a2263682f8ecae7e49562eb700c09374371491
Sarath <Sarath_Vankadaru@epam.com>
2020-02-11 00:05:37 +0530

    completed draw rectangle

--- task/HW-01/src/draw-rectangle.js
+++ task/HW-01/src/draw-rectangle.js
@@ -18,12 +18,36 @@
  *            '└┘\n'
  *
  *             '┌──────────┐\n'+
- *  (12,3) =>  '│          │\n'+
+ *  (12,3) =>  '│          |\n'+
  *             '└──────────┘\n'
  *
  */
 function drawRectangle(width, height) {
-    throw new Error('Not implemented');
+    let rectangle = "";
+
+    for (let h = 0; h < height; h++) {
+      for (let w = 0; w < width; w++) {
+        if (h == 0 && w == 0) {
+          rectangle += "┌";
+        } else if (h == height - 1 && w == 0) {
+          rectangle += "└";
+        } else if (h == 0 && w == width - 1) {
+          rectangle += "┐";
+        } else if (h == height - 1 && w == width - 1) {
+          rectangle += "┘";
+        } else if ( (h == 0 || h == height - 1) && w > 0 && w < width - 1) {
+          rectangle += "─";
+        } else if ( (w == 0 || w == width - 1) && h > 0 && h < height - 1) {
+          rectangle += "│";
+        } else if (w == width) {
+          rectangle += "\n";
+        } else {
+          rectangle += " ";
+        }
+      }
+      rectangle += "\n";
+    }
+    return rectangle;
 }
 
 
