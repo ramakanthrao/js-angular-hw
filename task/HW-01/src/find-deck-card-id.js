82c5e122fd8dc3dfd92800710c7d6fcd0187d907
Sridhar Yamsani <Sridhar_Yamsani@epam.com>
2020-02-10 14:12:49 +0530

    deck card changes

--- task/HW-01/src/find-deck-card-id.js
+++ task/HW-01/src/find-deck-card-id.js
@@ -23,7 +23,72 @@
  *   'K♠' => 51
  */
 function getCardId(value) {
-    throw new Error('Not implemented');
+    var suitValue = value.charAt(0);
+    var suit = value.charAt(1);
+    var firstValue;
+    var secondValue;
+    if(suitValue == '1'){
+        suit = value.charAt(2);
+    }
+    switch(suitValue) {
+    case 'A':
+        firstValue = 0;
+        break;
+    case '2':
+        firstValue =  1;
+        break;
+    case '3':
+        firstValue =  2;
+        break;
+    case '4':
+        firstValue =  3;
+        break;
+    case '5':
+        firstValue =  4;
+        break;
+    case '6':
+        firstValue =  5;
+        break;
+    case '7':
+        firstValue =  6;
+        break;
+    case '8':
+        firstValue =  7;
+        break;
+    case '9':
+        firstValue =  8;
+        break;
+    case '1':
+        firstValue =  9;
+        break;
+    case 'J':
+        firstValue =  10;
+        break;
+    case 'Q':
+        firstValue =  11;
+        break;
+    case 'K':
+        firstValue =  12;
+        break;
+         
+    } 
+
+    switch(suit) {
+        case '♣':
+            secondValue = 0;
+            break;
+        case '♦':
+            secondValue =  13;
+            break;
+        case '♥':
+            secondValue =  26;
+            break;
+        case '♠':
+            secondValue =  39;
+            break;
+                     
+        } 
+        return (firstValue + secondValue);
 }
 
 
