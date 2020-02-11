efca2fe1601c577f484a1c7e5b593ef85ead3548
Karnakar Gadegoni <Karnakar_Gadegoni@epam.com>
2020-02-11 11:50:40 +0530

    Updated the code for mem

--- task/HW-02/src/functions-closures.js
+++ task/HW-02/src/functions-closures.js
@@ -85,7 +85,11 @@ function getPolynom() {
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
 function memoize(func) {
-    throw new Error('Not implemented');
+    let mem;
+    if(mem === null || mem === undefined){
+        mem=func();
+    }
+    return () => mem;
 }
 
 
