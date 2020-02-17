16c53eeb6083fcb05812455cf316b254dcc6c8b8
Naveen.Baddhipadige <naveen_baddhipadige@epam.com>
2020-02-17 14:32:22 +0530

    HW-01 complete

--- task/HW-01/src/draw-rectangle.js
+++ task/HW-01/src/draw-rectangle.js
@@ -23,7 +23,29 @@
  *
  */
 function drawRectangle(width, height) {
-    throw new Error('Not implemented');
+    var finalString = '';
+    for (var i = 1; i <= height; i++) {
+        for (var j = 1; j <= width; j++) {
+            if (i == 1 && j == 1)
+                finalString += '\u250c';
+            else if (i == 1 && j == width)
+                finalString += '\u2510' + '\n';
+            else if (i == height && j == 1)
+                finalString += '\u2514';
+            else if (i == height && j == width)
+                finalString += '\u2518';
+            else if ((i == 1 || i == height) && (j > 1 && j < width))
+                finalString += '\u2500';
+            else if ((j == 1) && (i > 1 && i < height))
+                finalString += '\u2502';
+            else if ((j == width) && (i > 1 && i < height))
+                finalString += '\u2502' + '\n';
+            else if ((j > 1 && j < width) && (i > 1 && i < height))
+                finalString += ' ';
+        }
+    }
+    return finalString+'\n';
+
 }
 
 
