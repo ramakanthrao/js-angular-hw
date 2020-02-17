c7fdfd9b3976184f4090ec325f8764e948603763
Ramakanth Kulkarni <Ramakanth_Kulkarni@epam.com>
2020-02-17 17:57:44 +0530

    updated users list

--- bin/users.js
+++ bin/users.js
@@ -1,17 +1,24 @@
 'use strict';
-const users = [  "Venkatarao_Gude",
-                  "Akbar_Mohmad",
-                  "Vedadri_Kakarla",
-                  "Nishant_Ranjan",
-                  "Jitendra_Pandey",
-                  "Ismail_Shaik",
-                  "Karnakar_Gadegoni",
-                  "Suneetha_Pusuluri",
-                  "Sarath_Vankadaru",
-                  "Devendar_Reddy_Gadiam",
-                  "Naveen_Baddhipadige",
-                  "Sridhar_Yamsani",
-                  "Anjaneyelu_Neerati",
-                  "Rakesh_Budugu"
-                ];
+const users = [
+				"Nishant_Ranjan",
+				"Sarath_Vankadaru",
+				"Shubhendu_Parhi",
+				"Pratyush_Chhibber",
+				"Apurv_Vaidya",
+				"Naveen_Baddhipadige",
+				"Anjaneyelu_Neerati",
+				"Ismail_Shaik",
+				"Sridhar_Yamsani",
+				"Vedadri_Kakarla",
+				"Nirmal_Jayadharan",
+				"Akbar_Mohmad",
+				"Devendar_Reddy_Gadiam",
+				"Chandan_Singh",
+				"Pujitha_Gajula",
+				"Suneetha_Pusuluri",
+				"Karnakar_Gadegoni",
+				"Venkatarao_Gude",
+				"Rakesh_Budugu",
+				"Saurabh_Mishra"
+			];
 module.exports = users;              
