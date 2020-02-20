b51893490bf2194a707440bfc5988ac29a44ed81
Ramakanth Kulkarni <Ramakanth_Kulkarni@epam.com>
2020-02-20 12:11:53 +0530

    updated few ui test cases

---
+++ task/HW-04/test/karma-html.spec.js
@@ -0,0 +1,18 @@
+describe("Card class should render",function(){
+  it("Card name should appear ",function(){
+
+	document.body.onload = function() {
+		var card = new Card('Total Users', 22, 'fa-users', 'border-left-success');
+		document.body.innerHTML = card.template();
+		expect(document.querySelector('#name-1')).toContainText('Total Users');
+		//done();
+	}; });
+  
+  it("Card value should appear",function(){
+	document.body.onload = function() {
+		var card = new Card('Total Users', 22, 'fa-users', 'border-left-success');
+		document.body.innerHTML = card.template();
+		expect(document.querySelector('#value-1')).toContainText('22');
+	}
+  });
+});
